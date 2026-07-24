dadb437dec2b36acda0a0828a077bc9fb879280e
Abhinav <AbhinavSunil@hotmail.com>
2026-07-24 10:50:45 +0530

    Feat : Added pydantic validation

--- app/validations/pydanticValidation.py
+++ app/validations/pydanticValidation.py
@@ -1,3 +1,31 @@
 from pydantic import BaseModel,Field
-import json
+from typing import Any,Optional,List
+from enum import Enum
 
+class Difficulty(str,Enum):
+    EASY = "easy"
+    MEDIUM = "medium"
+    HARD = "hard"
+
+class Language(str,Enum):
+    Python = "python"
+    Java = "java"
+    Cpp = "cpp"
+
+class TestCaseInputs(BaseModel):
+    input : Any = Field(...,description="The test input case (can be string, number, array, or object)")
+
+class CodeSnippet(BaseModel):
+    language : Language
+    startSnippet: str = Field(..., description="Boilerplate start code (e.g. 'def solve(s: str) -> bool:')")
+    midSnippet: Optional[str] = Field("", description="Boilerplate middle template code")
+    endSnippet: str = Field(..., description="Closing boilerplate template code")
+
+class GeneratedProblemRaw(BaseModel):
+    title: str = Field(..., description="A unique and descriptive title of the challenge")
+    description: str = Field(..., description="Detailed description in markdown format explaining the task, constraints, and inputs")
+    difficulty: Difficulty
+    reference_solution: str = Field(..., description="A fully working, optimal Python 3 solution to the problem")
+    testCaseInputs: List[TestCaseInputs] = Field(..., description="A list of 3-5 logical input cases to test")
+    codeSnippets: List[CodeSnippet] = Field(..., description="Starter templates for standard languages")
+    topic: Optional[str] = Field(None, description="Topic tag (e.g., 'Array', 'String')")
