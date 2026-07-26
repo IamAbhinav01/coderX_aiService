bb6955813f8a092d32291a946a847c70ccd0f2a5
Abhinav <AbhinavSunil@hotmail.com>
2026-07-26 14:05:48 +0530

    Working on fastapi server

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI
-
-
-
-
-
+from fastapi import FastAPI,HTTPException,status
+from pydantic import BaseModel,Field
 
 app = FastAPI(
-    title="CoderX AI Service API"
+    title="CoderX AI Service API",
     description="Generate dynamic , fully verified coding challenges with refernce solutions and test cases.",
-    version="1.0.0"
-)
+    version="1.0.0",)
+
+class ProblemRequest(BaseModel):
+    prompt : str = Field(description="The topic or prompt for generating the problem"
+                         )
+
+@app.post("/api/v1/generate-problem",status_code=status.HTTP_202_ACCEPTED)
+def generate_problem(body:ProblemRequest):
+    try:
+        payload = 
+    except:
