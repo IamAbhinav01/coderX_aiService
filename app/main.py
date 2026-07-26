84e7c1d5b3853103a81df885f646df4be32a0812
Abhinav <AbhinavSunil@hotmail.com>
2026-07-26 13:56:43 +0530

    Feat : working on fastapi to create api endpoints

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+
+
+
+
+
+app = FastAPI(
+    title="CoderX AI Service API"
+    description="Generate dynamic , fully verified coding challenges with refernce solutions and test cases.",
+    version="1.0.0"
+)
