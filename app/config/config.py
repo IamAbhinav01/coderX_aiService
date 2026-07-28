28f9ed095e5362b53a97794da6e629b4a2a29abf
Abhinav <AbhinavSunil@hotmail.com>
2026-07-28 23:18:32 +0530

    Feat : Pydantic way of working on .env=model_config

--- app/config/config.py
+++ app/config/config.py
@@ -1,4 +1,15 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    
+
+    APP_NAME:str = "CoderX_AI_SERVICE"
+    ENV:str = "development"
+    PORT:int = 8000
+    GROQ_API_KEY:str
+    GROQ_MODEL:str = "llama-3.3-70b-versatile"
+    GROQ_MAX_TOKENS:int = 4096
+    TEMPERATURE:float = 0.7
+    PINECONE_API_KEY:str
+    PINECONE_INDEX_NAME:str = "coderx"
+    PINECONE_NAMESPACE:str = "coding_Prompts"
+    HF_TOKEN:str
