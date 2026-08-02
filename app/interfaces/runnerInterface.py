3eb48ed2b99cce7f6e86c345ba155a8b35ba3845
Abhinav <AbhinavSunil@hotmail.com>
2026-08-03 00:10:23 +0530

    Feat : Added the interface for subprocess execution

--- app/interfaces/runnerInterface.py
+++ app/interfaces/runnerInterface.py
@@ -1,5 +1,8 @@
 from abc import ABC,abstractmethod
-from typing import Optional,Dict,Any
+from typing import List,Dict,Any
+from models.model import GeneratedProblemRaw
 
 class InterfaceRunner(ABC):
-    
+    @abstractmethod
+    def verify_and_execute(self,raw_problem:GeneratedProblemRaw)->List[Dict[str,Any]]:
+        pass
