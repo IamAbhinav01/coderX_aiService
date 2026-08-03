9c10b7e5f474ecd0093f818c485af95634290326
Abhinav <AbhinavSunil@hotmail.com>
2026-08-03 10:07:37 +0530

    Feat : Added testcase subprocess

--- app/services/subprocessRunner.py
+++ app/services/subprocessRunner.py
@@ -1,7 +1,10 @@
 from models.model import GeneratedProblemRaw
 from interfaces.runnerInterface import InterfaceRunner
 from typing import Optional,Any,List
+from config.logger import setup_logger
 import json,subprocess,sys
+
+logger = setup_logger()
 
 class SubProcessRunner(InterfaceRunner):
     def verify_and_execute(self, raw_problem:GeneratedProblemRaw)->List[str,Any]:
@@ -17,7 +20,16 @@ class SubProcessRunner(InterfaceRunner):
                                     stderr=subprocess.PIPE,
                                     text=True
                 )
-            stdout,stderr = process.communicate(input_str,timeout=2.5)
-            output = stdout.strip() if stdout else ""
-            if process.returncode == 0 and output:
-                
+                stdout,stderr = process.communicate(input_str,timeout=2.5)
+                output = stdout.strip() if stdout else ""
+                if process.returncode == 0 and output:
+                    validated_cases.append({
+                        "input":input_str,
+                        "output":output
+                    })
+            except Exception as e:
+                logger.error(f"Subprocess runner error: {e}")
+                if 'process' in locals() and process.poll() is None:
+                    process.kill()
+                continue
+        return validated_cases
